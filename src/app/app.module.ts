e9c6e82c780433959f7f5b7c1de986e65cc0d3bb
najjar <mohamed_najjar@uff.net>
2017-08-02 18:08:33 +0200

    fff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // <-- import required BrowserAnimationsModule
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // <-- import required BrowserAnimationsModule
 // Import your library
 import { CollapsibleModule } from 'angular2-collapsible'; // <-- import the module
-
-import {AppComponent} from './app.component';
-import { ErreurTraceComponent } from './erreur-trace/erreur-trace.component';
 import { TraceErreurComponent } from './trace-erreur/trace-erreur.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ErreurTraceComponent,
     TraceErreurComponent
   ],
+    bootstrap: [TraceErreurComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,  // <-- include required BrowserAnimationsModule
     CollapsibleModule // <-- include angular2-collapsible module
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
